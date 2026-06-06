a0a31e5b5eee832c48109f3acd13a7f822f51444
Yamim Ali <nnjjstudiojj@gmail.com>
2026-06-06 17:40:20 +0100

    I refactored the gamePanel class to prepare it for my chess game

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -1,33 +1,13 @@
 import java.awt.Color;
 import java.awt.Dimension;
-
 import javax.swing.JPanel;
 
 public class gamePanel extends JPanel{
 
-    private static final int ROWS = 25;
-    private static final int COLS = 25;
-    private static final int CELL_SIZE = 24;
-    private static final int width = 600;
-    private static final int height = 600;
+    
+    public gamePanel () { 
 
-    public gamePanel() {
-        
-        this.setBackground(Color.BLACK);
-        this.setPreferredSize(new Dimension(width, height));
-
-        repaint();
-    }
-
-    @Override
-    protected void paintComponent(java.awt.Graphics g) {
-        super.paintComponent(g);
-
-        g.setColor(Color.GREEN);
-        
-        for (int i = 0; i <= ROWS; i++) {
-            g.drawLine(0, i * CELL_SIZE, width - 1, i * CELL_SIZE);
-            g.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, height - 1);
-        }  
+        setPreferredSize(new Dimension(1000, 700));
+        setBackground(Color.WHITE);
     }
 }
