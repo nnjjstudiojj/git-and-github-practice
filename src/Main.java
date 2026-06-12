b65422a499e0501b17b0f28fa29d41fcd2b6dcb9
Yamim Ali <nnjjstudiojj@gmail.com>
2026-06-12 22:24:53 +0100

    created the game menu java class and made some changes to the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +7,8 @@ public class Main {
         JFrame frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
 
-        frame.add(new gamePanel());
+        JMenu menu = new JMenu();
+        frame.add(menu); 
 
         frame.pack();
         frame.setResizable(false);
