ab6d5ddfbc3176e5bddff77421c9dc083143cd8d
Yamim Ali <nnjjstudiojj@gmail.com>
2026-06-13 12:29:05 +0100

    deleted the gamesMenu class and changed the Main class to create a new menu

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Snake Game");
+        frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
 
-        JMenu menu = new JMenu();
-        frame.add(menu); 
+        JMenuBar menuBar = new JMenuBar();
 
-        frame.pack();
-        frame.setResizable(false);
+        JMenu gamesMenu = new JMenu("Games");
+
+        JMenuItem snakeGame = new JMenuItem("Snake");
+        JMenuItem pongGame = new JMenuItem("Pong");
+        JMenuItem chessGame = new JMenuItem("Chess");
+
+        snakeGame.addActionListener(e -> System.out.println("Running snake game"));
+        chessGame.addActionListener(e -> System.out.println("Running chess game"));
+        pongGame.addActionListener(e -> System.out.println("Running pong game"));
+
+        gamesMenu.add(snakeGame);
+        gamesMenu.add(pongGame);
+        gamesMenu.add(chessGame);
+
+        menuBar.add(gamesMenu);
+        frame.setJMenuBar(menuBar);
+         
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
